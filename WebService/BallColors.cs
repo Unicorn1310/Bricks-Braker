ed80f03ac4eb826223bbbb9a0820692f015ddccc
Unicorn1310 <96735345+Unicorn1310@users.noreply.github.com>
2021-12-28 13:04:22 +0200

    Add files via upload

---
+++ WebService/BallColors.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebService
+{
+    public class BallColors
+    {
+        //מחלקת צבע כדור
+        private int BallColorId; // הID של צבע הכדור
+        private string BallColor; //צבע הכדור
+        private int LogInHisId; //הID של היסטוריית ההתחברות
+
+        public BallColors()
+        {
+
+        }
+
+        public int ballColorId // הID של צבע הכדור
+        {
+            set
+            {
+                this.BallColorId = value;
+            }
+            get
+            {
+                return this.BallColorId;
+            }
+        }
+
+        public string ballColor //צבע הכדור
+        {
+            set
+            {
+                this.BallColor = value;
+            }
+            get
+            {
+                return this.BallColor;
+            }
+        }
+
+        public int logInHisId //הID של היסטוריית ההתחברות
+        {
+            set
+            {
+                this.LogInHisId = value;
+            }
+            get
+            {
+                return this.LogInHisId;
+            }
+        }
+    }
+}
---
+++ WebService/LogInHis.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebService
+{
+    public class LogInHis
+    {
+        //מחלקת היסטוריית ההתחברות
+        private int LogInHisId; //הID של היסטוריית ההתחברות
+        private string UserName; //שם משתמש
+        private string Date; //תאריך
+        private string Time; //שעה
+
+        public LogInHis()
+        {
+
+        }
+
+        public int logInHisId //הID של היסטוריית ההתחברות
+        {
+            set
+            {
+                this.LogInHisId = value;
+            }
+            get
+            {
+                return this.LogInHisId;
+            }
+        }
+
+        public string userName //שם משתמש
+        {
+            set
+            {
+                this.UserName = value;
+            }
+            get
+            {
+                return this.UserName;
+            }
+        }
+
+        public string date //תאריך
+        {
+            set
+            {
+                this.Date = value;
+            }
+            get
+            {
+                return this.Date;
+            }
+        }
+
+        public string time //שעה
+        {
+            set
+            {
+                this.Time = value;
+            }
+            get
+            {
+                return this.Time;
+            }
+        }
+    }
+}
